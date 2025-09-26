e63c4f700ec4cf7f10278096286c06812fbff9db
Timo <timon.black.gamig@gmail.com>
2025-09-26 20:31:47 +0200

    Add Konami code easter egg to display a deep-fried image
    - Implemented a new feature that listens for the Konami code input.
    - Upon successful input, an image is displayed in the bottom right corner of the screen.
    - The image is removed automatically after 5 seconds.

---
+++ static/js/konami.js
@@ -0,0 +1,15 @@
+import Konami from "konami";
+
+const easterEgg = new Konami(() => {
+  const img = document.createElement("img");
+  img.src = "/static/images/deepfried_1757435754587.jpeg";
+  img.style.position = "fixed";
+  img.style.bottom = "20px";
+  img.style.right = "20px";
+  img.style.width = "150px";
+  img.style.zIndex = 1000;
+  document.body.appendChild(img);
+
+  // Optional: remove after 5 seconds
+  setTimeout(() => img.remove(), 5000);
+});
